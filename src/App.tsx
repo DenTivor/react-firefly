52bd992233a05ee40b8c091f24211755f17f50e7
Roman Alekseev <forestincboard@gmail.com>
2021-02-06 18:07:55 +0300

    Add basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import {IProps} from './Models';
 import './styles/styles.styl';
 
-const App = (props: IProps) => <h2 className="mainHeader">{props?.title}</h2>;
+const App = () => {
+    return (
+        <div className="cover-page standard-background">
+            {/* Page Layer */}
+
+            <div className="standardCentralSection">
+            {/* Component Layer */}
+                <div className="standardCentralSection__inner">
+                    <h2 className="mainHeader">React firefly</h2>
+                </div>
+            {/* //Component Layer */}
+            </div>
+
+            {/* // Page Layer */}
+        </div>
+    )    
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const title = 'My Minimal React Webpack Babel Setup';
-
 ReactDOM.render(
-  <App title={title} />,
+  <App />,
   document.getElementById('app')
 );
